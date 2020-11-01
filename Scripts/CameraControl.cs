59633818e8973c4f81f47ca753e89f5fa5d406d5
Sha Liang <liangshaahu@gmail.com>
2020-11-01 20:07:57 +0000

    initial v1

---
+++ Scripts/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Camera cam;
+    private float rotateSpeed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+        cam.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+
+        if (Input.GetKey(KeyCode.R))
+        {
+
+            cam.transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+
+            cam.transform.Rotate(-Vector3.up, Time.deltaTime * rotateSpeed);
+
+
+        }
+    }
+}
---
+++ Scripts/EnginePlay.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class EnginePlay : MonoBehaviour
+{
+    public Animator engineAnimator;
+    public Slider sliderSpeed;
+
+
+
+    //public Button playButton;
+    //public Button stopButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        engineAnimator.SetFloat("animationSpeed", sliderSpeed.value);
+        //engineAnimator.speed = sliderSpeed.value;
+
+
+        if (Input.GetKeyDown("x") && engineAnimator.GetBool("IsPlay") == false)
+        {
+
+            engineAnimator.SetBool("IsPlay", true);
+        }
+        else if (Input.GetKeyDown("x") && engineAnimator.GetBool("IsPlay") == true)
+        {
+            engineAnimator.SetBool("IsPlay", false);
+
+        }
+
+    }
+
+    public void PlayAnimation ()
+    {
+        engineAnimator.SetBool("IsPlay", true);
+    }
+
+    public void StopAnimation()
+    {
+        engineAnimator.SetBool("IsPlay", false);
+    }
+
+}
